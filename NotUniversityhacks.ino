a8c21e3b71d816fa45760ad60048fc0bcd15358a
Ankit Gupta <gankit389@gmail.com>
2020-06-06 15:13:20 -0400

    added question cycling

--- NotUniversityhacks.ino
+++ NotUniversityhacks.ino
@@ -1,22 +1,48 @@
-struct questionGroup{
-    String question;
-    String choices[5];
-    int correctAnswer;
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(7, 8, 9, 10, 12, 13);
+
+const byte arraySize = 3;
+
+typedef struct  {
+  String question;
+  String choices[5];
+  int correctAnswer;
+} questionGroup;
+
+questionGroup question[arraySize] = {
+  {"what is ajdkshfjahsdjkf", {"AHSKJDF", "FDJKG", "SFDHGKJ", "HGFJHF", "DJFHGKJDH"}, 2},
+  {"what is what", {"AHSKJDF", "FDJKG", "SFDHGKJ", "HGFJHF", "DJFHGKJDH"}, 2},
+  {"what is hello", {"AHSKJDF", "FDJKG", "SFDHGKJ", "HGFJHF", "DJFHGKJDH"}, 2}
 };
 
-struct questionGroup question1 ={"what is ajdkshfjahsdjkf",{"AHSKJDF","FDJKG","SFDHGKJ","HGFJHF","DJFHGKJDH"}, 2};
-
-
-
-
+bool answered = false;
+int questionNumber = -1;
 
 void setup() {
   Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.clear();
 
 }
 
 void loop() {
-  Serial.print(question1.choices[2]);
-  delay(50000);
+
+  answered = false;
+
+  while (answered == false) {
+    // check if a option is selected, if so set answered to true so the loop breaks
+    delay(5000);
+    break;
+  }
+  questionNumber++;
+  printNextQuestion(questionNumber);
+
+}
+
+void printNextQuestion(int index) {
+  lcd.clear();
+  String currentQuestion = question[index].question;
+  lcd.print(currentQuestion);
+
 
 }
