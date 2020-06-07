65c4e50ed9a488798d71b957135bbee919ab3da4
Ankit Gupta <gankit389@gmail.com>
2020-06-07 00:32:20 -0400

    first commit (website)

---
+++ main.js
@@ -0,0 +1,49 @@
+class Question {
+    question;
+    choices;
+    correctAnswer;
+
+    constructor(question, choices, correctAnswer) {
+        this.question = question;
+        this.choices = choices;
+        this.correctAnswer = correctAnswer;
+    }
+
+}
+
+var textBox = "YOUR CODE"
+var allQuestions = [];
+// onsubmit() =>
+/*
+    var question = document.getById('question').value;
+    var choices = array;
+    choices.push(document.getById('choices1').value);
+        choices.push(document.getById('choices1').value);
+
+            choices.push(document.getById('choices1').value);
+
+                choices.push(document.getById('choices1').value);
+
+                    choices.push(document.getById('choices1').value);
+    var correctAnswer = documnenadfopjfask=-flw
+
+    var entry = new Question(question, choices, correctAnswer);
+    allQuestions.push(entry);
+    
+
+
+
+    for (allQuestions) {
+        add to the text box
+        call the method to below
+        
+        textBox += convertQuestionsToCode(question);
+    }
+
+
+    document.getElementById('textBox').value = textBox
+*/
+
+convertQuestionsToCode = (question) => {
+    //Convert to code
+}
